d896eb95d9081e8eaa8835a97367a572f4c89b02
Hydrogen <61741153+pjy02@users.noreply.github.com>
2024-08-28 15:08:38 +0800

    Update release-rule-set.sh

--- .github/release-rule-set.sh
+++ .github/release-rule-set.sh
@@ -8,7 +8,8 @@ cd sing-geoip/rule-set
 git init
 git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/lyc8503/sing-box-rules.git
+# 更改远程仓库 URL 和使用自定义的 PAT
+git remote add origin https://github-action:${GITHUB_TOKEN}@github.com/pjy02/sing-box-rules.git
 git branch -M rule-set-geoip
 git add .
 git commit -m "Update rule-set"
@@ -21,7 +22,8 @@ cd sing-geosite/rule-set
 git init
 git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/lyc8503/sing-box-rules.git
+# 更改远程仓库 URL 和使用自定义的 PAT
+git remote add origin https://github-action:${GITHUB_TOKEN}@github.com/pjy02/sing-box-rules.git
 git branch -M rule-set-geosite
 git add .
 git commit -m "Update rule-set"
